94f6483ff655c8206695502353a031b3d89e5e68
melisa-gisel-gimenez <melisag@live.com.ar>
2023-12-03 22:55:04 -0300

    agrego form del super usuario

---
+++ PPSuperUs.designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace arreglarTesis
+{
+    partial class PPSuperUs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PPSuperUs));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mentoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reunionesDeMentoriasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.postulacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.acercaDeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pastorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ministeriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.administracionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tesoreriaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inhabilitarMiembrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asignarMentorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.altaUsuarioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.revisarPostuladosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.altaMentorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarMentorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.altaMinisteriosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.miembrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.altaDeMiembrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificacionDeMiembrosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarIngresosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.agregarEgresosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.generarInformesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(831, 521);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.menuStrip1.Font = new System.Drawing.Font("Calibri", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mentoresToolStripMenuItem,
+            this.personasToolStripMenuItem,
+            this.pastorToolStripMenuItem,
+            this.ministeriosToolStripMenuItem,
+            this.listasToolStripMenuItem,
+            this.administracionToolStripMenuItem,
+            this.tesoreriaToolStripMenuItem,
+            this.acercaDeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.menuStrip1.Size = new System.Drawing.Size(831, 32);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mentoresToolStripMenuItem
+            // 
+            this.mentoresToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.reunionesDeMentoriasToolStripMenuItem,
+            this.postulacionesToolStripMenuItem});
+            this.mentoresToolStripMenuItem.Name = "mentoresToolStripMenuItem";
+            this.mentoresToolStripMenuItem.Size = new System.Drawing.Size(103, 29);
+            this.mentoresToolStripMenuItem.Text = "Mentores";
+            // 
+            // reunionesDeMentoriasToolStripMenuItem
+            // 
+            this.reunionesDeMentoriasToolStripMenuItem.Name = "reunionesDeMentoriasToolStripMenuItem";
+            this.reunionesDeMentoriasToolStripMenuItem.Size = new System.Drawing.Size(332, 34);
+            this.reunionesDeMentoriasToolStripMenuItem.Text = "Reuniones de mentorias";
+            // 
+            // postulacionesToolStripMenuItem
+            // 
+            this.postulacionesToolStripMenuItem.Name = "postulacionesToolStripMenuItem";
+            this.postulacionesToolStripMenuItem.Size = new System.Drawing.Size(332, 34);
+            this.postulacionesToolStripMenuItem.Text = "Postulaciones de miembros";
+            // 
+            // acercaDeToolStripMenuItem
+            // 
+            this.acercaDeToolStripMenuItem.Name = "acercaDeToolStripMenuItem";
+            this.acercaDeToolStripMenuItem.Size = new System.Drawing.Size(105, 29);
+            this.acercaDeToolStripMenuItem.Text = "Acerca de";
+            // 
+            // personasToolStripMenuItem
+            // 
+            this.personasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.inhabilitarMiembrosToolStripMenuItem,
+            this.asignarMentorToolStripMenuItem});
+            this.personasToolStripMenuItem.Name = "personasToolStripMenuItem";
+            this.personasToolStripMenuItem.Size = new System.Drawing.Size(98, 29);
+            this.personasToolStripMenuItem.Text = "Personas";
+            // 
+            // pastorToolStripMenuItem
+            // 
+            this.pastorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.altaUsuarioToolStripMenuItem,
+            this.revisarPostuladosToolStripMenuItem,
+            this.altaMentorToolStripMenuItem,
+            this.modificarMentorToolStripMenuItem});
+            this.pastorToolStripMenuItem.Name = "pastorToolStripMenuItem";
+            this.pastorToolStripMenuItem.Size = new System.Drawing.Size(77, 29);
+            this.pastorToolStripMenuItem.Text = "Pastor";
+            // 
+            // ministeriosToolStripMenuItem
+            // 
+            this.ministeriosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.altaMinisteriosToolStripMenuItem});
+            this.ministeriosToolStripMenuItem.Name = "ministeriosToolStripMenuItem";
+            this.ministeriosToolStripMenuItem.Size = new System.Drawing.Size(114, 29);
+            this.ministeriosToolStripMenuItem.Text = "Ministerios";
+            // 
+            // listasToolStripMenuItem
+            // 
+            this.listasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miembrosToolStripMenuItem});
+            this.listasToolStripMenuItem.Name = "listasToolStripMenuItem";
+            this.listasToolStripMenuItem.Size = new System.Drawing.Size(71, 29);
+            this.listasToolStripMenuItem.Text = "Listas";
+            // 
+            // administracionToolStripMenuItem
+            // 
+            this.administracionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.altaDeMiembrosToolStripMenuItem,
+            this.modificacionDeMiembrosToolStripMenuItem});
+            this.administracionToolStripMenuItem.Name = "administracionToolStripMenuItem";
+            this.administracionToolStripMenuItem.Size = new System.Drawing.Size(147, 29);
+            this.administracionToolStripMenuItem.Text = "Administracion";
+            // 
+            // tesoreriaToolStripMenuItem
+            // 
+            this.tesoreriaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.agregarIngresosToolStripMenuItem,
+            this.agregarEgresosToolStripMenuItem,
+            this.generarInformesToolStripMenuItem});
+            this.tesoreriaToolStripMenuItem.Name = "tesoreriaToolStripMenuItem";
+            this.tesoreriaToolStripMenuItem.Size = new System.Drawing.Size(97, 29);
+            this.tesoreriaToolStripMenuItem.Text = "Tesoreria";
+            // 
+            // inhabilitarMiembrosToolStripMenuItem
+            // 
+            this.inhabilitarMiembrosToolStripMenuItem.Name = "inhabilitarMiembrosToolStripMenuItem";
+            this.inhabilitarMiembrosToolStripMenuItem.Size = new System.Drawing.Size(278, 34);
+            this.inhabilitarMiembrosToolStripMenuItem.Text = "Inhabilitar miembros";
+            // 
+            // asignarMentorToolStripMenuItem
+            // 
+            this.asignarMentorToolStripMenuItem.Name = "asignarMentorToolStripMenuItem";
+            this.asignarMentorToolStripMenuItem.Size = new System.Drawing.Size(278, 34);
+            this.asignarMentorToolStripMenuItem.Text = "Asignar mentor";
+            // 
+            // altaUsuarioToolStripMenuItem
+            // 
+            this.altaUsuarioToolStripMenuItem.Name = "altaUsuarioToolStripMenuItem";
+            this.altaUsuarioToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.altaUsuarioToolStripMenuItem.Text = "Alta usuario";
+            // 
+            // revisarPostuladosToolStripMenuItem
+            // 
+            this.revisarPostuladosToolStripMenuItem.Name = "revisarPostuladosToolStripMenuItem";
+            this.revisarPostuladosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.revisarPostuladosToolStripMenuItem.Text = "Revisar postulados";
+            // 
+            // altaMentorToolStripMenuItem
+            // 
+            this.altaMentorToolStripMenuItem.Name = "altaMentorToolStripMenuItem";
+            this.altaMentorToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.altaMentorToolStripMenuItem.Text = "Alta Mentor";
+            // 
+            // modificarMentorToolStripMenuItem
+            // 
+            this.modificarMentorToolStripMenuItem.Name = "modificarMentorToolStripMenuItem";
+            this.modificarMentorToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.modificarMentorToolStripMenuItem.Text = "Modificar Mentor";
+            // 
+            // altaMinisteriosToolStripMenuItem
+            // 
+            this.altaMinisteriosToolStripMenuItem.Name = "altaMinisteriosToolStripMenuItem";
+            this.altaMinisteriosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.altaMinisteriosToolStripMenuItem.Text = "Alta ministerios";
+            // 
+            // miembrosToolStripMenuItem
+            // 
+            this.miembrosToolStripMenuItem.Name = "miembrosToolStripMenuItem";
+            this.miembrosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.miembrosToolStripMenuItem.Text = "Miembros";
+            // 
+            // altaDeMiembrosToolStripMenuItem
+            // 
+            this.altaDeMiembrosToolStripMenuItem.Name = "altaDeMiembrosToolStripMenuItem";
+            this.altaDeMiembrosToolStripMenuItem.Size = new System.Drawing.Size(327, 34);
+            this.altaDeMiembrosToolStripMenuItem.Text = "Alta de miembros";
+            // 
+            // modificacionDeMiembrosToolStripMenuItem
+            // 
+            this.modificacionDeMiembrosToolStripMenuItem.Name = "modificacionDeMiembrosToolStripMenuItem";
+            this.modificacionDeMiembrosToolStripMenuItem.Size = new System.Drawing.Size(327, 34);
+            this.modificacionDeMiembrosToolStripMenuItem.Text = "Modificacion de miembros";
+            // 
+            // agregarIngresosToolStripMenuItem
+            // 
+            this.agregarIngresosToolStripMenuItem.Name = "agregarIngresosToolStripMenuItem";
+            this.agregarIngresosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.agregarIngresosToolStripMenuItem.Text = "Agregar Ingresos";
+            // 
+            // agregarEgresosToolStripMenuItem
+            // 
+            this.agregarEgresosToolStripMenuItem.Name = "agregarEgresosToolStripMenuItem";
+            this.agregarEgresosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.agregarEgresosToolStripMenuItem.Text = "Agregar Egresos";
+            // 
+            // generarInformesToolStripMenuItem
+            // 
+            this.generarInformesToolStripMenuItem.Name = "generarInformesToolStripMenuItem";
+            this.generarInformesToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.generarInformesToolStripMenuItem.Text = "Generar informes";
+            // 
+            // PPSuperUs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 521);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "PPSuperUs";
+            this.Text = "MCD Gestion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mentoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reunionesDeMentoriasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem postulacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem acercaDeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem inhabilitarMiembrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asignarMentorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pastorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altaUsuarioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem revisarPostuladosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altaMentorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarMentorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ministeriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altaMinisteriosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem miembrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem administracionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem altaDeMiembrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificacionDeMiembrosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tesoreriaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarIngresosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem agregarEgresosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem generarInformesToolStripMenuItem;
+    }
+}
