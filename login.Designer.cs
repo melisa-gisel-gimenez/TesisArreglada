5f3deb1ea5a44cc4e495945485a902b8b36c09c8
julieta turco <julivickiturco23@gmail.com>
2023-12-04 23:29:15 -0300

    cambios de logo en login

--- login.Designer.cs
+++ login.Designer.cs
@@ -34,18 +34,18 @@
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.buttonCerrar = new System.Windows.Forms.Button();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pbOcultar = new System.Windows.Forms.PictureBox();
             this.pbMostrar = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbOcultar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMostrar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnIniciarSesion
@@ -60,10 +60,10 @@
             this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnIniciarSesion.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIniciarSesion.ForeColor = System.Drawing.Color.Black;
-            this.btnIniciarSesion.Location = new System.Drawing.Point(300, 169);
-            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(4);
+            this.btnIniciarSesion.Location = new System.Drawing.Point(338, 211);
+            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(236, 52);
+            this.btnIniciarSesion.Size = new System.Drawing.Size(266, 65);
             this.btnIniciarSesion.TabIndex = 3;
             this.btnIniciarSesion.Text = "Iniciar Sesion";
             this.btnIniciarSesion.UseVisualStyleBackColor = false;
@@ -71,32 +71,32 @@
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(281, 70);
+            this.txtUsername.Location = new System.Drawing.Point(316, 88);
             this.txtUsername.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(277, 22);
+            this.txtUsername.Size = new System.Drawing.Size(311, 26);
             this.txtUsername.TabIndex = 1;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(281, 119);
+            this.txtPassword.Location = new System.Drawing.Point(316, 149);
             this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(277, 22);
+            this.txtPassword.Size = new System.Drawing.Size(311, 26);
             this.txtPassword.TabIndex = 2;
             this.txtPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // buttonCerrar
             // 
-            this.buttonCerrar.BackColor = System.Drawing.Color.Transparent;
+            this.buttonCerrar.BackColor = System.Drawing.Color.Chocolate;
             this.buttonCerrar.FlatAppearance.BorderSize = 0;
             this.buttonCerrar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.LightSalmon;
             this.buttonCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
             this.buttonCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonCerrar.Location = new System.Drawing.Point(567, 0);
+            this.buttonCerrar.Location = new System.Drawing.Point(641, 0);
             this.buttonCerrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonCerrar.Name = "buttonCerrar";
-            this.buttonCerrar.Size = new System.Drawing.Size(53, 38);
+            this.buttonCerrar.Size = new System.Drawing.Size(60, 48);
             this.buttonCerrar.TabIndex = 10;
             this.buttonCerrar.Text = "X";
             this.buttonCerrar.UseVisualStyleBackColor = false;
@@ -105,35 +105,24 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(408, 149);
+            this.linkLabel1.Location = new System.Drawing.Point(459, 186);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(141, 16);
+            this.linkLabel1.Size = new System.Drawing.Size(168, 20);
             this.linkLabel1.TabIndex = 11;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Recuperar contraseña";
             this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.SandyBrown;
-            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.panel1.Controls.Add(this.buttonCerrar);
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(623, 38);
-            this.panel1.TabIndex = 12;
-            // 
             // pictureBox2
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.SandyBrown;
             this.pictureBox2.ErrorImage = null;
-            this.pictureBox2.Image = global::arreglarTesis.Properties.Resources.logo;
+            this.pictureBox2.Image = global::arreglarTesis.Properties.Resources.Sin_título__520_x_258_px___253_x_300_px_;
             this.pictureBox2.InitialImage = null;
             this.pictureBox2.Location = new System.Drawing.Point(0, 0);
             this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(225, 240);
+            this.pictureBox2.Size = new System.Drawing.Size(253, 300);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
@@ -142,10 +131,10 @@
             // pbOcultar
             // 
             this.pbOcultar.Image = ((System.Drawing.Image)(resources.GetObject("pbOcultar.Image")));
-            this.pbOcultar.Location = new System.Drawing.Point(564, 113);
+            this.pbOcultar.Location = new System.Drawing.Point(634, 141);
             this.pbOcultar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pbOcultar.Name = "pbOcultar";
-            this.pbOcultar.Size = new System.Drawing.Size(36, 32);
+            this.pbOcultar.Size = new System.Drawing.Size(40, 40);
             this.pbOcultar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbOcultar.TabIndex = 7;
             this.pbOcultar.TabStop = false;
@@ -154,10 +143,10 @@
             // pbMostrar
             // 
             this.pbMostrar.Image = ((System.Drawing.Image)(resources.GetObject("pbMostrar.Image")));
-            this.pbMostrar.Location = new System.Drawing.Point(564, 113);
+            this.pbMostrar.Location = new System.Drawing.Point(634, 141);
             this.pbMostrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pbMostrar.Name = "pbMostrar";
-            this.pbMostrar.Size = new System.Drawing.Size(36, 32);
+            this.pbMostrar.Size = new System.Drawing.Size(40, 40);
             this.pbMostrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pbMostrar.TabIndex = 6;
             this.pbMostrar.TabStop = false;
@@ -166,10 +155,10 @@
             // pictureBox4
             // 
             this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
-            this.pictureBox4.Location = new System.Drawing.Point(231, 113);
+            this.pictureBox4.Location = new System.Drawing.Point(260, 141);
             this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(40, 33);
+            this.pictureBox4.Size = new System.Drawing.Size(45, 41);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 5;
             this.pictureBox4.TabStop = false;
@@ -177,20 +166,30 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(231, 63);
+            this.pictureBox3.Location = new System.Drawing.Point(260, 79);
             this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(40, 34);
+            this.pictureBox3.Size = new System.Drawing.Size(45, 42);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 4;
             this.pictureBox3.TabStop = false;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::arreglarTesis.Properties.Resources.Sin_título__520_x_258_px___2_;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(701, 50);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
             // LoginJuli
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(622, 240);
+            this.ClientSize = new System.Drawing.Size(700, 300);
+            this.Controls.Add(this.buttonCerrar);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.txtPassword);
@@ -198,24 +197,24 @@
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.btnIniciarSesion);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pbMostrar);
             this.Controls.Add(this.pbOcultar);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MaximumSize = new System.Drawing.Size(622, 240);
-            this.MinimumSize = new System.Drawing.Size(622, 240);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(700, 300);
+            this.MinimumSize = new System.Drawing.Size(700, 300);
             this.Name = "LoginJuli";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "login";
             this.Load += new System.EventHandler(this.LoginJuli_Load);
-            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbOcultar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbMostrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -232,7 +231,7 @@
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button buttonCerrar;
         private System.Windows.Forms.LinkLabel linkLabel1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
