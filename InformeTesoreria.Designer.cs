2911f51848aa24fe088e36a5184fb766a37d34f8
julieta turco <julivickiturco23@gmail.com>
2023-12-05 00:18:45 -0300

    cambios esteticos informe tesoreria

--- InformeTesoreria.Designer.cs
+++ InformeTesoreria.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle15 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InformeTesoreria));
             this.DGVIngresos = new System.Windows.Forms.DataGridView();
             this.dateTimePickerDesde = new System.Windows.Forms.DateTimePicker();
             this.dateTimePickerHasta = new System.Windows.Forms.DateTimePicker();
@@ -42,55 +43,69 @@
             this.label4 = new System.Windows.Forms.Label();
             this.btnSalir = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.DGVIngresos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.DGVEgresos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // DGVIngresos
             // 
             this.DGVIngresos.AllowUserToAddRows = false;
             this.DGVIngresos.AllowUserToDeleteRows = false;
-            this.DGVIngresos.BackgroundColor = System.Drawing.SystemColors.ControlLight;
-            dataGridViewCellStyle15.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle15.BackColor = System.Drawing.Color.Teal;
-            dataGridViewCellStyle15.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle15.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            dataGridViewCellStyle15.SelectionBackColor = System.Drawing.SystemColors.MenuBar;
-            dataGridViewCellStyle15.SelectionForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            dataGridViewCellStyle15.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.DGVIngresos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle15;
+            this.DGVIngresos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.DGVIngresos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DGVIngresos.BackgroundColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Teal;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Calibri Light", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.MenuBar;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DGVIngresos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.DGVIngresos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DGVIngresos.EnableHeadersVisualStyles = false;
-            this.DGVIngresos.Location = new System.Drawing.Point(16, 157);
+            this.DGVIngresos.Location = new System.Drawing.Point(45, 232);
+            this.DGVIngresos.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.DGVIngresos.Name = "DGVIngresos";
             this.DGVIngresos.ReadOnly = true;
             this.DGVIngresos.RowHeadersWidth = 51;
             this.DGVIngresos.RowTemplate.Height = 24;
-            this.DGVIngresos.Size = new System.Drawing.Size(477, 150);
+            this.DGVIngresos.Size = new System.Drawing.Size(590, 226);
             this.DGVIngresos.TabIndex = 0;
             // 
             // dateTimePickerDesde
             // 
-            this.dateTimePickerDesde.Location = new System.Drawing.Point(70, 48);
+            this.dateTimePickerDesde.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerDesde.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerDesde.Location = new System.Drawing.Point(261, 103);
+            this.dateTimePickerDesde.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.dateTimePickerDesde.Name = "dateTimePickerDesde";
-            this.dateTimePickerDesde.Size = new System.Drawing.Size(189, 22);
+            this.dateTimePickerDesde.Size = new System.Drawing.Size(150, 26);
             this.dateTimePickerDesde.TabIndex = 1;
             this.dateTimePickerDesde.ValueChanged += new System.EventHandler(this.dateTimePickerDesde_ValueChanged);
             // 
             // dateTimePickerHasta
             // 
             this.dateTimePickerHasta.Enabled = false;
-            this.dateTimePickerHasta.Location = new System.Drawing.Point(70, 76);
+            this.dateTimePickerHasta.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerHasta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerHasta.Location = new System.Drawing.Point(261, 145);
+            this.dateTimePickerHasta.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.dateTimePickerHasta.Name = "dateTimePickerHasta";
-            this.dateTimePickerHasta.Size = new System.Drawing.Size(189, 22);
+            this.dateTimePickerHasta.Size = new System.Drawing.Size(150, 26);
             this.dateTimePickerHasta.TabIndex = 2;
             this.dateTimePickerHasta.ValueChanged += new System.EventHandler(this.dateTimePickerHasta_ValueChanged);
             // 
             // btnFiltrar
             // 
-            this.btnFiltrar.Location = new System.Drawing.Point(345, 48);
+            this.btnFiltrar.Location = new System.Drawing.Point(453, 122);
+            this.btnFiltrar.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.btnFiltrar.Name = "btnFiltrar";
-            this.btnFiltrar.Size = new System.Drawing.Size(89, 30);
+            this.btnFiltrar.Size = new System.Drawing.Size(95, 37);
             this.btnFiltrar.TabIndex = 3;
             this.btnFiltrar.Text = "Filtrar";
             this.btnFiltrar.UseVisualStyleBackColor = true;
@@ -100,18 +115,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 48);
+            this.label1.Font = new System.Drawing.Font("Microsoft Yi Baiti", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(190, 103);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.Size = new System.Drawing.Size(56, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "Desde:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 82);
+            this.label2.Font = new System.Drawing.Font("Microsoft Yi Baiti", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(190, 153);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.Size = new System.Drawing.Size(52, 20);
             this.label2.TabIndex = 5;
             this.label2.Text = "Hasta:";
             // 
@@ -119,31 +136,36 @@
             // 
             this.DGVEgresos.AllowUserToAddRows = false;
             this.DGVEgresos.AllowUserToDeleteRows = false;
-            this.DGVEgresos.BackgroundColor = System.Drawing.Color.Gainsboro;
-            dataGridViewCellStyle16.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle16.BackColor = System.Drawing.Color.IndianRed;
-            dataGridViewCellStyle16.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle16.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            dataGridViewCellStyle16.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle16.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle16.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.DGVEgresos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle16;
+            this.DGVEgresos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.DGVEgresos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.DGVEgresos.BackgroundColor = System.Drawing.Color.SteelBlue;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.IndianRed;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Calibri Light", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.DGVEgresos.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.DGVEgresos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.DGVEgresos.EnableHeadersVisualStyles = false;
-            this.DGVEgresos.Location = new System.Drawing.Point(16, 383);
+            this.DGVEgresos.Location = new System.Drawing.Point(45, 540);
+            this.DGVEgresos.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.DGVEgresos.Name = "DGVEgresos";
             this.DGVEgresos.ReadOnly = true;
             this.DGVEgresos.RowHeadersWidth = 51;
             this.DGVEgresos.RowTemplate.Height = 24;
-            this.DGVEgresos.Size = new System.Drawing.Size(477, 150);
+            this.DGVEgresos.Size = new System.Drawing.Size(590, 226);
             this.DGVEgresos.TabIndex = 6;
             // 
             // btnDescargar
             // 
             this.btnDescargar.Enabled = false;
-            this.btnDescargar.Location = new System.Drawing.Point(70, 563);
+            this.btnDescargar.Font = new System.Drawing.Font("Calibri Light", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDescargar.Location = new System.Drawing.Point(133, 810);
+            this.btnDescargar.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.btnDescargar.Name = "btnDescargar";
-            this.btnDescargar.Size = new System.Drawing.Size(117, 45);
+            this.btnDescargar.Size = new System.Drawing.Size(147, 67);
             this.btnDescargar.TabIndex = 7;
             this.btnDescargar.Text = "Descargar";
             this.btnDescargar.UseVisualStyleBackColor = true;
@@ -152,26 +174,30 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 135);
+            this.label3.Font = new System.Drawing.Font("Calibri Light", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(20, 203);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.Size = new System.Drawing.Size(82, 24);
             this.label3.TabIndex = 8;
             this.label3.Text = "Ingresos:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 352);
+            this.label4.Font = new System.Drawing.Font("Calibri Light", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 511);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 16);
+            this.label4.Size = new System.Drawing.Size(77, 24);
             this.label4.TabIndex = 9;
             this.label4.Text = "Egresos:";
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(267, 563);
+            this.btnSalir.Font = new System.Drawing.Font("Calibri Light", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(401, 810);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(117, 45);
+            this.btnSalir.Size = new System.Drawing.Size(147, 67);
             this.btnSalir.TabIndex = 10;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -181,17 +207,36 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(152, 9);
+            this.label5.Location = new System.Drawing.Point(40, 51);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(172, 20);
+            this.label5.Size = new System.Drawing.Size(221, 25);
             this.label5.TabIndex = 11;
             this.label5.Text = "Reporte de Tesorería:";
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::arreglarTesis.Properties.Resources._61;
+            this.pictureBox2.Location = new System.Drawing.Point(303, -5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(374, 259);
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::arreglarTesis.Properties.Resources._51;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 690);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 260);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
             // InformeTesoreria
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(529, 631);
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(678, 947);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.label4);
@@ -204,10 +249,20 @@
             this.Controls.Add(this.dateTimePickerHasta);
             this.Controls.Add(this.dateTimePickerDesde);
             this.Controls.Add(this.DGVIngresos);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Calibri Light", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.MaximumSize = new System.Drawing.Size(700, 1003);
+            this.MinimumSize = new System.Drawing.Size(700, 1003);
             this.Name = "InformeTesoreria";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "InformeTesoreria";
             ((System.ComponentModel.ISupportInitialize)(this.DGVIngresos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.DGVEgresos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,5 +282,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btnSalir;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
